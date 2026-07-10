f0135e72d8e319fa762aef97985625affa29c34c
Saurabh Kumar Bajpai <saurabhkumarbajpaiai@gmail.com>
2026-07-10 12:40:57 +0530

    fix: remove unused React import in Home component

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiMap, FiSearch, FiCalendar, FiFileText, FiAlertCircle, FiHome, FiUsers, FiMessageSquare } from 'react-icons/fi';
 import { Footer } from './ui/footer';
